089f26e798ef75852c12e2f914194a9d23df6966
storm <stormaw07@gmail.com>
2025-03-25 17:49:37 +0100

    Dropdown menu for account
    Added a dropdown menu in the header for logging in or creating an account. Moved the game fields out of the slogan box and moved it below it

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Dropdown menu:
-function dropDownMenu() {
-    document.getElementById("dropdownMusic").classList.toggle("show");
-  }
+function dropDownMenuMusic() {
+  document.getElementById("dropdownMusic").classList.toggle("show")
+}
+function dropDownMenuAccount() {
+  document.getElementById("dropDownAccount").classList.toggle("show")
+}
