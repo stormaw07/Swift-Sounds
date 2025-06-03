785562cf8b35b4b04a93914e52a08a48a748ac96
storm <stormaw07@gmail.com>
2025-06-03 13:46:16 +0200

    Added merch to the header (dropdownmenu) and made the merch pages
    Added a merch dropdown menu to the header which displays links to music, other and cart when pressed. Also created the html files and file-structure for the merch shop

--- script.js
+++ script.js
@@ -3,26 +3,39 @@
 let body = document.getElementById('body')
 let musicMenu = document.getElementById('dropdownMusic')
 let accountMenu = document.getElementById('dropDownAccount')
+let merchMenu = document.getElementById('dropDownMerch')
 
 
 function dropDownMenuMusic() {
   musicMenu.classList.toggle("show")
   accountMenu.classList.remove("show")
+  merchMenu.classList.remove("show")
 }
 
 function dropDownMenuAccount() {
   accountMenu.classList.toggle("show")
   musicMenu.classList.remove("show")
+  merchMenu.classList.remove("show")
+}
+
+function dropDownMenuMerch() {
+  merchMenu.classList.toggle("show")
+  musicMenu.classList.remove("show")
+  accountMenu.classList.remove("show")
 }
 
 document.addEventListener('click', function (event) {
   const isClickInsideMusic = musicMenu.contains(event.target) || document.getElementById('headerMusic').contains(event.target)
   const isClickInsideAccount = accountMenu.contains(event.target) || document.getElementById('headerAccount').contains(event.target)
+  const isClickInsideMerch = merchMenu.contains(event.target) || document.getElementById('headerMerch').contains(event.target)
 
   if (!isClickInsideMusic) {
     musicMenu.classList.remove("show")
   }
   if (!isClickInsideAccount) {
     accountMenu.classList.remove("show")
+  }
+  if (!isClickInsideMerch) {
+    merchMenu.classList.remove("show")
   }
 })
