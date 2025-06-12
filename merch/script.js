9702d6a52baf4d2bbaf369bb110a371b462d9635
storm <stormaw07@gmail.com>
2025-06-12 10:19:28 +0200

    Added the addtocart function for the merch shop
    Added a function that takes the page in as an argument and adds the pressed item to the localstorage cart. If the same item is pressed multiple times its quantity is increased instead of adding it another time

--- merch/script.js
+++ merch/script.js
@@ -1,4 +1,5 @@
 let shop = document.getElementById('shopItems');
+let merch = '';
 
 function getMerchItems(type) {
     const API_URL = 'http://localhost:3000/'+type //Henter ut enten merch eller other gjennm en onload i bodyen, basert på hvilken side du er på 
@@ -6,6 +7,7 @@ function getMerchItems(type) {
     fetch(API_URL)
     .then (response => response.json())
     .then (data => {
+        merch = data; //Setter den globale variabelen merch til å være data, sånn at den kan bli brukt utenfor blokken
         console.log(data);
 
         for (let i=0; i<data.length; i++) {
@@ -21,3 +23,31 @@ function getMerchItems(type) {
         console.log(error)
     })
 };
+
+function addToCart(item){
+    console.log(merch[item]) //Sjekker om den returnerer det itemet man trykker på
+
+    let newItem = {
+        id: merch[item].id,
+        name: merch[item].name,
+        url: merch[item].url,
+        price: merch[item].price,
+        quantity: 1
+    };
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    let existingItem = cart.find(cartItem => cartItem.id === newItem.id)
+
+    if (existingItem){
+        existingItem.quantity += 1;
+    } else{
+        cart.push(newItem);
+    };
+
+    console.log(cart);
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+};
+
+//localStorage.removeItem('cart'); //For å tømem carten
